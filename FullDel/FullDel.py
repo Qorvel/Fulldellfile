a5c5344b2538cd7920b43ed35d891b8a2c6bc6f3
Qorvel <123703598+Qorvel@users.noreply.github.com>
2024-09-20 18:41:46 +0300

    1.01

---
+++ FullDel/FullDel.py
@@ -0,0 +1,28 @@
+import os
+import random
+
+class FullDel:
+
+    di = os.listdir()
+    trash = []
+
+    for i in di:
+        if os.path.isdir(i) == False and i != "FullDel.exe":
+            trash.append(i)
+
+    for i in trash:
+        new = ""
+
+
+        file = open(i,"rb")
+        reader = file.read()
+        file.close()
+        for j in range(50):
+            ran = random.random()
+            ran = str(ran)[3]
+            overwrite = str(ran)*len(reader)
+
+            file = open(i,"w")
+            file.write(overwrite)
+            file.close()
+        os.remove(i)
